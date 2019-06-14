d8194e8f39ccac419328a569fa23dc85138f50f1
Dipesh1997 <dipeshkataria3@gmail.com>
2019-06-14 07:23:34 +0530

    Added Cropme in App

---
+++ app/src/main/java/com/example/tryyourdesign/models/Album.java
@@ -0,0 +1,18 @@
+package com.example.tryyourdesign.models;
+
+import java.util.List;
+
+/**
+ * Created by takusemba on 2017/09/10.
+ */
+
+public class Album {
+
+    public String bucketId;
+
+    public String name;
+
+    public List<Photo> photos;
+
+    public boolean isSelected = false;
+}
---
+++ app/src/main/java/com/example/tryyourdesign/ui/OnPhotoClickListener.java
@@ -0,0 +1,12 @@
+package com.example.tryyourdesign.ui;
+
+import com.example.tryyourdesign.models.Photo;
+
+/**
+ * Created by takusemba on 2017/09/11.
+ */
+
+public interface OnPhotoClickListener {
+
+    void onPhotoClicked(Photo photo);
+}
