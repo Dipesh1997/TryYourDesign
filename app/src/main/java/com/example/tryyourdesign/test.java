75144b08d4b4220f11f4f9ca89e356bd3fc298b7
Dipesh1997 <dipeshkataria3@gmail.com>
2019-07-09 21:10:51 +0530

    Added test page

---
+++ app/src/main/java/com/example/tryyourdesign/test.java
@@ -0,0 +1,52 @@
+package com.example.tryyourdesign;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tryyourdesign.ui.activities.SplashActivity;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+    public void onMainActivity(View view)
+    {
+        Intent intent = new Intent(test.this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void onPaint(View view)
+    {
+        Intent intent = new Intent(test.this, PaintACT.class);
+        startActivity(intent);
+    }
+    public void onPaintActivity(View view)
+    {
+        Intent intent = new Intent(test.this, PaintingActivity.class);
+        startActivity(intent);
+    }
+    public void onPolygon(View view)
+    {
+        Intent intent = new Intent(test.this, Polygon.class);
+        startActivity(intent);
+    }
+    public void onHNB(View view)
+    {
+        Intent intent = new Intent(test.this, HorizontalNtbActivity.class);
+        startActivity(intent);
+    }
+    public void onMainActivity2(View view)
+    {
+        Intent intent = new Intent(test.this, MainActivity2.class);
+        startActivity(intent);
+    }
+    public void onSplashActivity(View view)
+    {
+        Intent intent = new Intent(test.this, SplashActivity.class);
+        startActivity(intent);
+    }
+}
