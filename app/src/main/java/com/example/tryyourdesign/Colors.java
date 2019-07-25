b66a6ada5cad69c86d414a3ce460c3c9c86d108a
Dipesh1997 <dipeshkataria3@gmail.com>
2019-07-25 07:12:58 +0530

    Added Json Support

---
+++ app/src/main/java/com/example/tryyourdesign/Colors.java
@@ -0,0 +1,74 @@
+package com.example.tryyourdesign;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Colors extends AppCompatActivity {
+
+    // ArrayList for person names, email Id's and mobile numbers
+    ArrayList<String> personNames = new ArrayList<>();
+    ArrayList<String> mobileNumbers = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+        // get the reference of RecyclerView
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        try {
+            // get JSONObject from JSON file
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            // fetch JSONArray named users
+            JSONArray userArray = obj.getJSONArray("colorEntry");
+            // implement for loop for getting users list data
+            for (int i = 0; i < userArray.length(); i++) {
+                // create a JSONObject for fetching single user data
+                JSONObject userDetail = userArray.getJSONObject(i);
+                // fetch email and name and store it in arraylist
+                personNames.add(userDetail.getString("colorName"));
+                // create a object for getting contact data from JSONObject
+                JSONObject contact = userDetail.getJSONObject("RGB8");
+                // fetch mobile number and store it in arraylist
+                mobileNumbers.add(contact.getString("red"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //  call the constructor of CustomAdapter to send the reference and data to Adapter
+        CustomAdapter customAdapter = new CustomAdapter(Colors.this, personNames, mobileNumbers);
+        recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("data.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+}
+
